79f4997df74ab87e5939e7191a63d90e7ff3830a
Pradish <pradishvaidyas@gmail.com>
2022-05-10 22:36:09 +0530

    changes

--- main.py
+++ main.py
@@ -51,6 +51,24 @@ def calccv(x: float, y: float):
 
 @client.event
 async def on_ready():
+    await client.change_presence(status=discord.Status.dnd,activity=discord.Game('Made by prad ❤️'))
+    print("We have logged in sucessfully as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if "<@872795608770052116>" in message.content:
+        await message.channel.send("Prefix is %! For help say %help_<command name>.This bot has currently only two features one is %cv and %ping. More features will be released soon!! Stay tuned")
+    
+@client.command()
+async def help_ping(ctx):
+    await ctx.send(f"Shows the current ping in your discord client")
+
+@client.command()
+async def help_cv(ctx):
+    await ctx.send(f"Syntax: %cv <crit rate> <crit value>")
+
+@client.event
+async def on_ready():
     print("Up and running")
 
 @client.event
