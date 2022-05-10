f843f13c0163c1e84963a616288ab78aac8b1773
Pradish <pradishvaidyas@gmail.com>
2022-05-10 22:50:01 +0530

    changes

--- main.py
+++ main.py
@@ -51,14 +51,8 @@ def calccv(x: float, y: float):
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.dnd,activity=discord.Game('Made by prad ❤️'))
-    print("We have logged in sucessfully as {0.user}".format(client))
+    print("Up and running")
 
-@client.event
-async def on_message(message):
-    if "<@872795608770052116>" in message.content:
-        await message.channel.send("Prefix is %! For help say %help_<command name>.This bot has currently only two features one is %cv and %ping. More features will be released soon!! Stay tuned")
-    
 @client.command()
 async def help_ping(ctx):
     await ctx.send(f"Shows the current ping in your discord client")
@@ -67,21 +61,10 @@ async def help_ping(ctx):
 async def help_cv(ctx):
     await ctx.send(f"Syntax: %cv <crit rate> <crit value>")
 
-@client.event
-async def on_ready():
-    print("Up and running")
-
-@client.event
-async def on_member_join(member):
-    print(f'Welcome to our server! {member}')
-
-@client.event
-async def on_member_remove(member):
-    print(f'See ya soon! {member}')
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f"{round(client.latency*100)}ms")
+    await ctx.send(f"{round(client.latency*1000)}ms")
 
 @client.command(aliases=["cv"])
 
