f21d4e58f2dfcd96a2948780cc0f9f8415b09110
Pradish <pradishvaidyas@gmail.com>
2022-05-11 19:18:10 +0530

    twitter integration

--- main.py
+++ main.py
@@ -1,50 +1,17 @@
-"""from ast import alias
-from pydoc import cli
-import os
-import discord
-from discord.ext import commands
-
-intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
-client = commands.Bot(command_prefix="%", intents=intents)
-
-def calccv(x: float, y: float):
-    return x*2+y
-
-@client.event
-async def on_ready():
-    await client.change_presence(status=discord.Status.dnd,activity=discord.Game('Made by prad ❤️'))
-    print("We have logged in sucessfully as {0.user}".format(client))
-
-@client.event
-async def on_message(message):
-    if "<@872795608770052116>" in message.content:
-        await message.channel.send("Prefix is %! For help say %help_<command name>.This bot has currently only two features one is %cv and %ping. More features will be released soon!! Stay tuned")
-    
-@client.command()
-async def help_ping(ctx):
-    await ctx.send(f"Shows the current ping in your discord client")
-
-@client.command()
-async def help_cv(ctx):
-    await ctx.send(f"Syntax: %cv <crit rate> <crit value>")
-
-@client.command()
-async def ping(ctx):
-    await ctx.send(f"{round(client.latency*100)}ms")
-
-@client.command(aliases=["cv"])
-async def critvalue(ctx, x: float, y: float):
-    cv=calccv(x,y)
-    await ctx.send(f"Your crit value is {cv} !")
-"""
 from ast import alias
 from pydoc import cli
 import os
 import discord
 from discord.ext import commands
+import tweepy
+
 
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix="%", intents=intents)
+auth = tweepy.OAuth2AppHandler(os.environ["consumer_key","consumer_secret"])
+api = tweepy.API(auth)
+user=api.get_user(screen_name="GenshinImpact")
+
 
 def calccv(x: float, y: float):
     return x*2+y
@@ -67,10 +34,14 @@ async def ping(ctx):
     await ctx.send(f"{round(client.latency*1000)}ms")
 
 @client.command(aliases=["cv"])
-
 async def critvalue(ctx, x: float, y: float):
     cv=calccv(x,y)
     await ctx.send(f"Your crit value is {cv}")
+
+#tweepy intergation
+@client.command()
+async def info(ctx):
+    await ctx.send(f"{user.screen_name} has {user.followers_count} followers")
 
 client.run(os.environ["token"])
 
