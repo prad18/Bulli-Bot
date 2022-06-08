a60d57866e40250ee165519080af85f8314f3420
Pradish <pradishvaidyas@gmail.com>
2022-06-08 14:37:29 +0530

    commit

--- tweet.py
+++ tweet.py
@@ -4,7 +4,7 @@ import os
 #live tweets from a specific user
 def get_tweets(username):
     auth = tweepy.OAuthHandler(os.environ["consumer_key"], os.environ["consumer_secret"])
-    auth.set_access_token(os.enviorn["access_token"], os.environ["access_token_secret"])
+    auth.set_access_token(os.environ["access_token"], os.environ["access_token_secret"])
     api = tweepy.API(auth)
     tweets = api.user_timeline(screen_name=username, count=1, include_rts=False,exclude_replies=True)
     return tweets
