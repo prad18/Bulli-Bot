ebdba6feb1d266fbab469d6548cacaa7e592da7f
Pradish <pradishvaidyas@gmail.com>
2022-06-16 13:33:23 +0530

    completed i think...

--- tweet.py
+++ tweet.py
@@ -9,18 +9,22 @@ def get_tweets(username):
     tweets = api.user_timeline(screen_name=username, count=1, include_rts=False,exclude_replies=True)
     return tweets
 #tweet urls from a specific user
+#tweet urls from a specific user
 def get_tweet_urls(username):
     tweets = get_tweets(username)
     urls = []
     for tweet in tweets:
         if len(tweet.entities['urls']) > 0:
-            urls.append(tweet.entities['urls'][0]['expanded_url'])
-        elif ['media'] in tuple(tweet.entities):
-            print(tweet.entities['media'][0]['media_url'])
-        elif len(tweet.entities['media']) > 0:
-            urls.append(tweet.entities['media'][0]['expanded_url'])
+            a=(tweet.entities['urls'][0]['expanded_url'])
+            if "https://twitter.com/" not in a:
+                urls.append(tweet.entities['urls'][1]['expanded_url'])
+            else:
+                urls.append(tweet.entities['urls'][0]['expanded_url'])
+        elif 'media' in tuple(tweet.entities):
+            if len(tweet.entities['media']) > 0:
+                urls.append(tweet.entities['media'][0]['expanded_url'])
+            else:
+                print(tweet.entities['media'][0]['expanded_url'])
         else:
-            urls.append(str(tweet.text))
-        for url in urls:
-            print(str(url))           
+            print(tweet.text)
     return urls
