628dd9e0a2a6411ee13b33cca0eeca1f1163832b
Pradish <pradishvaidyas@gmail.com>
2022-06-21 14:56:16 +0530

    commit2.0

--- genshin.py
+++ genshin.py
@@ -1,9 +1,5 @@
 gen=[]
 gen2=[]
-
-def user_word(u):
-    with open("genshin.txt") as f:
-        for line in f:
-            gen.append(line.strip())
-    if u in gen:
-        print("Yes")
+with open("genshin.txt") as f:
+    for line in f:
+        gen.append(line.strip())
