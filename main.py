c1d083d57d6cd406ece612c70c8bdfb6fc8e2b41
Pradish <pradishvaidyas@gmail.com>
2022-06-22 22:50:28 +0530

    commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import tweepy
 from tweet import get_tweet_urls
 from genshin import *
 
-intents = nextcord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
+intents = nextcord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True , message_content=True)
 client = commands.Bot(command_prefix="%", intents=intents,status=nextcord.Status.dnd,activity=nextcord.Game(name="%help"))
 auth = tweepy.OAuth2AppHandler(os.environ["consumer_key"],os.environ["consumer_secret"])
 api = tweepy.API(auth)
