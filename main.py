65f8241846905ae141f950dad0ab8bc2bff47af0
Pradish <pradishvaidyas@gmail.com>
2022-06-22 11:51:02 +0530

    commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import discord
 from discord.ext import commands
 import tweepy
 from tweet import get_tweet_urls
-from genshin import *
+import genshin
 
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix="%", intents=intents)
