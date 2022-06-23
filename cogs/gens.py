dc948d4bcfee835482a776273dab8f2a0f3846f6
Pradish <pradishvaidyas@gmail.com>
2022-06-24 00:58:31 +0530

    commit

---
+++ cogs/gens.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+from genshin import gen
+
+def calccv(x: float, y: float):
+    return x*2+y
+
+
+class Genshin(commands.Cog):
+    @commands.command(aliases=["cv"])
+    async def critvalue(ctx, x: float, y: float):
+        cv=calccv(x,y)
+        await ctx.send(f"Your crit value is {cv}")
+
+
+    @commands.Cog.listener()
+    async def on_message(self,message):
+        for i in gen:
+            if i in message.content:
+                await message.channel.send("Yes")
+                break
+
+
+def setup(client):
+    client.add_cog(Genshin(client))
--- main.py
+++ main.py
@@ -32,10 +32,7 @@ async def help_cv(ctx):
 async def ping(ctx):
     await ctx.send(f"{round(client.latency*1000)}ms")
 
-@client.command(aliases=["cv"])
-async def critvalue(ctx, x: float, y: float):
-    cv=calccv(x,y)
-    await ctx.send(f"Your crit value is {cv}")
+
 
 
 @client.command()
@@ -51,13 +48,5 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-#@client.event
-#async def on_message(message):
-#    if message.author==client.user:
-#        return
-#    for i in gen:
-#        if i in message.content:
-#            await message.channel.send("Yes")
-#           break
 
 client.run(os.environ["token"])
