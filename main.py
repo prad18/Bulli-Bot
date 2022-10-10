e77b79a9ab60b76a8e569110f0a14a812ab1b40e
Pradish <pradishvaidyas@gmail.com>
2022-10-10 11:44:22 +0530

    commit

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ from genshin import *
 import asyncio
 
 
-
-intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
-client = commands.Bot(command_prefix="%", intents=intents,status=discord.Status.dnd,activity=discord.Game(name="%help"), )
+client = commands.Bot(command_prefix="%", intents=discord.Intents.all(),status=discord.Status.dnd,activity=discord.Game(name="%help"), )
 auth = tweepy.OAuth2AppHandler(os.environ["consumer_key"],os.environ["consumer_secret"])
 api = tweepy.API(auth)
 
@@ -25,22 +23,22 @@ async def on_ready():
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f"{round(client.latency*1000)}ms")
+    await ctx.reply(f"{round(client.latency*1000)}ms")
 
 @client.command()
 async def term2(ctx,theory:float,practicals:float,term1:float):
     y=term2_marks(theory,practicals,term1)
-    await ctx.send(f"Your term2 marks is: {y}")
+    await ctx.reply(f"Your term2 marks is: {y}")
 
 @client.command()
 async def load(ctx,extension):
     client.load_extension(f'cogs.{extension}')
-    await ctx.send("Loaded successfully")
+    await ctx.reply("Loaded successfully")
 
 @client.command()
 async def unload(ctx,extension):
     client.unload_extension(f'cogs.{extension}')
-    await ctx.send("Unloaded successfully")
+    await ctx.reply("Unloaded successfully")
 
 async def load_extensions():
     for filename in os.listdir('./cogs'):
